fe939d4c0922b340a9c1ec66e9fbd7620bd49e85
cosinechen <tree7035612@yahoo.com.tw>
2017-07-05 12:09:43 +0800

    try

---
+++ upload.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="zh-tw" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>上傳檔案</title>
+</head>
+
+<body>
+
+<form action="upload2.php" enctype="multipart/form-data" method="post">
+	<p>檔案上傳</p>
+	<p>
+	<input name="File1" type="file" style="width: 270px; height: 25px" />
+	<input name="Submit1" type="submit" value="送出" style="width: 70px; height: 25px" /></p>
+	<p>
+	&nbsp;</p>
+</form>
+
+</body>
+
+</html>
